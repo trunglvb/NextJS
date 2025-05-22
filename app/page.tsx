6241e455e5f2c97ef09d6c7511d4812c31ce3191
trunglvb <levubaotrung0311@gmail.com>
2025-05-22 21:25:48 +0700

    [Authentication-1]: config client env

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { clientEnvConfig } from "@/config";
 
 export default function Home() {
+	console.log(clientEnvConfig);
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
---
+++ config.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+// Các biến không phải NEXT_PUBLIC_môi trường chỉ khả dụng trong môi trường Node.js, nghĩa là trình duyệt không thể truy cập chúng (máy khách chạy trong một môi trường khác ).
+
+const configClientEnv = z.object({
+	NEXT_PUBLIC_API_ENDPOINT: z.string(),
+	NEXT_PUBLIC_URL: z.string(),
+});
+
+const clientEnv = configClientEnv.safeParse({
+	NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+	NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+});
+
+if (!clientEnv.success) {
+	console.error(clientEnv.error.errors);
+	throw new Error("Invalid environment variables");
+}
+
+const clientEnvConfig = clientEnv.data;
+export { clientEnvConfig };
