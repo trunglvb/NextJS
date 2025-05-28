1ac89cbdc2d81b946f22b67c395d857616db75c8
trunglvb <levubaotrung0311@gmail.com>
2025-05-28 22:27:36 +0700

    [Authentication-7]: config middleware

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const privatePaths = ["/manage"];
+const authPaths = ["/login"];
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+	const accessToken = Boolean(request.cookies.get("accessToken")?.value);
+
+	// Chưa đăng nhập thì không cho vào private paths
+	if (
+		privatePaths.some((path) => pathname.startsWith(path)) &&
+		!accessToken
+	) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+	// Đăng nhập rồi thì không cho vào login/register nữa
+	if (authPaths.some((path) => pathname.startsWith(path)) && accessToken) {
+		return NextResponse.redirect(new URL("/me", request.url));
+	}
+	return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+	matcher: ["/manage/:path*", "/login"],
+};
