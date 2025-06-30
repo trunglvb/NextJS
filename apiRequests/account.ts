6f0f3da83044ceda083c26a62fb7cc71f0f56bd5
trunglvb <levubaotrung0311@gmail.com>
2025-06-30 22:54:05 +0700

    [Account-2]: define apis

--- apiRequests/account.ts
+++ apiRequests/account.ts
@@ -4,6 +4,7 @@ import {
 	AccountType,
 	ChangePasswordBodyType,
 } from "@/schemaValidations/account.schema";
+import { get } from "http";
 
 const accountApiRequests = {
 	getMe: () => http.get<ISuccessResponse<AccountType>>("/accounts/me"),
@@ -14,5 +15,14 @@ const accountApiRequests = {
 			"/accounts/change-password",
 			data
 		),
+	list: () => http.get<ISuccessResponse<AccountType[]>>("/accounts"),
+	addEmployee: (data: Omit<AccountType, "id">) =>
+		http.post<ISuccessResponse<AccountType>>("/accounts", data),
+	editEmployee: (id: number, data: Omit<AccountType, "id">) =>
+		http.put<ISuccessResponse<AccountType>>(`/accounts/${id}`, data),
+	getEmployeeDetail: (id: number) =>
+		http.get<ISuccessResponse<AccountType>>(`/accounts/${id}`),
+	deleteEmployee: (id: number) =>
+		http.delete<ISuccessResponse<AccountType>>(`/accounts/${id}`),
 };
 export default accountApiRequests;
